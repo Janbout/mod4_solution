7886d3b6bc712286c31ad96248f4d72ceda19785
Janbout <90203776+Janbout@users.noreply.github.com>
2021-10-09 18:19:05 +0300

    Update script.js

--- index.html/script.js
+++ index.html/script.js
@@ -17,7 +17,7 @@
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
       var firstLetter = names[i].charAt(0).toLowerCase();
- if (firstLetter === 'J') {
+ if (firstLetter === 'j') {
     byespeaker(names[i]);
   } else {
    hellospeaker(names[i]);
